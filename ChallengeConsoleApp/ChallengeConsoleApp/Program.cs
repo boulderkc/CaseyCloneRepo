62defad247975419e9219e7fa81be6957651f737
Casey <caseyparsons@yahoo.com>
2018-05-30 22:31:24 -0600

    Work in progress, finishing up the console todo items, but still need to do some clean-up of variable and function names

--- ChallengeConsoleApp/ChallengeConsoleApp/Program.cs
+++ ChallengeConsoleApp/ChallengeConsoleApp/Program.cs
@@ -1,15 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace ChallengeConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static string _theRetString;
+
+        static void Main()
         {
+            
+            List<int> intList = new List<int> { 1, 2, 3, 4, 5 };
+            // Sum the ints
+            Console.WriteLine($"Sum of ints: {SumListOfInts(intList).ToString()}");
+            // Sum the even ints
+            Console.WriteLine($"Sum of even ints: {SumEvenNumbers(intList).ToString()}");
+
+
+            // Get thing from HTTP client, place it in _theRetString
+            RunAsync().GetAwaiter().GetResult();
+            //await RunAsync();
+            Console.WriteLine($"DateTime from HTTP get: {_theRetString}");
+
+            // The thread delay settings for the two threads are in app.config, where they can be configured. 
+            //Console.WriteLine($"Thread delay 1 setting: {StaticUtilities.GetThreadDelay1Setting()}");
+            //Console.WriteLine($"Thread delay 2 setting: {StaticUtilities.GetThreadDelay2Setting()}");
+            RunTwoThreadsWithDelay(intList);
+ 
+
+            // pause for user input
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
+
+        public static void RunTwoThreadsWithDelay(List<int> intList)
+        {
+
+            //Console.WriteLine($"Thread delay 1 setting: {StaticUtilities.GetThreadDelay1Setting()}");
+            //Console.WriteLine($"Thread delay 2 setting: {StaticUtilities.GetThreadDelay2Setting()}");
+
+
+            // Create an array of Thread references.
+            Thread[] array = new Thread[2];
+            // New each thread with ParameterizedThreadStart. This will allow us to call the Start with an object (a struct, in this case), which we'll use to pass 
+            // our list of integers and our delay.
+            ParameterizedThreadStart start = new ParameterizedThreadStart(StaticUtilities.Write1);
+            array[0] = new Thread(start);
+            StaticUtilities.ListOfIntsAndDelayStruct firstThreadValues = new StaticUtilities.ListOfIntsAndDelayStruct(intList, StaticUtilities.GetThreadDelay1Setting());
+            array[0].Start(firstThreadValues);
+
+
+            array[1] = new Thread(start);
+            StaticUtilities.ListOfIntsAndDelayStruct secondThreadValues = new StaticUtilities.ListOfIntsAndDelayStruct(intList, StaticUtilities.GetThreadDelay2Setting());
+            array[1].Start(secondThreadValues);
+
+        }
+
+        public static int SumListOfInts(List<int> ints)
+        {
+            int ret = ints.Sum();
+            return ret;
+        }
+
+        public static int SumEvenNumbers(List<int> ints)
+        {
+            int ret = ints.Where(s => s % 2 == 0).Sum(); 
+                return ret;
+        }
+
+ 
+        static async Task RunAsync()
+        {
+            HttpClient client = new HttpClient();
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://ipv6.fiddler:59383/");
+
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Get the XXX TODO
+                _theRetString = await GetAllSessionsResultAsync("http://localhost:59383/api/allsessionslaunch", client);
+                Console.WriteLine(_theRetString);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // In order to make ReadAsAsync work, needed to get nuget AspNet.WebApi.Client package, "install-package Microsoft.AspNet.WebApi.Client"
+        static async Task<string> GetAllSessionsResultAsync(string path, HttpClient client)
+        {
+            _theRetString = "";
+            var response = await client.GetAsync(path, HttpCompletionOption.ResponseHeadersRead); 
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                _theRetString = await response.Content.ReadAsAsync<string>();
+            }
+            return _theRetString;
+        }
+
     }
 }
---
+++ ChallengeConsoleApp/ChallengeConsoleApp/StaticUtilities.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChallengeConsoleApp
+{
+    public class StaticUtilities
+    {
+        public struct ListOfIntsAndDelayStruct
+        {
+            public List<int> listOfInts { get; set; }
+            public int delayMilliseconds { get; set; }
+
+            public ListOfIntsAndDelayStruct(List<int> prop1, int prop2)
+            {
+                listOfInts = prop1;
+                delayMilliseconds = prop2; 
+            }
+        }
+
+        public static int GetThreadDelay1Setting()
+        {
+            var reader = new AppSettingsReader();
+            try
+            {
+                var delay1Setting = reader.GetValue("Thread1Delay", typeof(int));
+                return (int)delay1Setting;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Configurable setting Thread1Delay is missing from the application configuration (app.config) file. Default of 1000 will be used.");
+                return 1000;
+            }
+        }
+
+        public static int GetThreadDelay2Setting()
+        {
+            var reader = new AppSettingsReader();
+            try
+            {
+                var delay2Setting = reader.GetValue("Thread2Delay", typeof(int));
+                return (int)delay2Setting;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Configurable setting Thread2Delay is missing from the application configuration (app.config) file. Default of 500 will be used.");
+                return 500; 
+            }
+        }
+
+        public static void Write1(object info)
+        {
+            // This receives the value passed into the Thread.Start method.
+            ListOfIntsAndDelayStruct x = (ListOfIntsAndDelayStruct)info;
+
+            List<int> intList = new List<int>(x.listOfInts);
+            int sleepValue = x.delayMilliseconds;
+            foreach (var item in intList)
+            {
+                Thread.Sleep(sleepValue);
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+    }
+
+
+
+}
+
+
